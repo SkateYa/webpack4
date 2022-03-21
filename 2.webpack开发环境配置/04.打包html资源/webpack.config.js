71f8338962574f119a1f5939088459ac80ea794a
zhangxya <zhangxya@pukkasoft.cn>
2022-03-21 23:40:57 +0800

    清空上次打包的文件

--- 2.webpack开发环境配置/04.打包html资源/webpack.config.js
+++ 2.webpack开发环境配置/04.打包html资源/webpack.config.js
@@ -4,6 +4,7 @@
 */
 const { resolve } = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -25,6 +26,8 @@ module.exports = {
       // 复制 './src/index.html' 文件，并自动引入打包输出的所有资源（JS/CSS）
       template: './src/index.html',
     }),
+    // 每次打包的时候，打包目录都会遗留上次打包的文件，为了保持打包目录的纯净，我们需要在打包前将打包目录清空
+    new CleanWebpackPlugin(), // 引入插件
   ],
   mode: 'development',
 };
